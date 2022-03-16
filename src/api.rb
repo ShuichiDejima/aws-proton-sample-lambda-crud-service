793a66c585f42e304d3540666c4b797dbf58cf33
ShuichiDejima <dejima.s@world-wing.com>
2022-03-16 13:15:19 +0900

    2ndcommit

--- src/api.rb
+++ src/api.rb
@@ -79,7 +79,7 @@ def get_task(event:,context:)
   begin
     return {
         statusCode: 200,
-        body: "Hello TSUBASA"
+        body: "Hello TSUBASA - This is [get]"
     }
     input = GetTaskInput.new(event)
     unless input.valid?
